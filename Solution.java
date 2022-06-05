fc2b69e4e2ffe3ed9607271e86a68e6d9f24297c
amithasound <70346133+amithasound@users.noreply.github.com>
2022-06-05 12:36:46 -0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+         int[] intArray = new int[2];
+        for(int i = 0; i < nums.length;i++){
+            for(int j = 0; j < nums.length;j++){
+                 
+                if(nums[i] + nums[j] == target && i != j){
+                   intArray[0] = i;
+                    intArray[1] = j;
+                    return intArray;
+                }       
+             }   
+        }
+        return intArray;
+    }
+}
